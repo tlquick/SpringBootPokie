9cee8a03dd47021be38d49f8cbdc68db622ea56e
Tracy Quick <tlquick999@gmail.com>
2022-10-31 12:55:41 +1100

    fix access issues with DevTools

--- src/test/java/com/tlquick/pokie/PokieApplicationTests.java
+++ src/test/java/com/tlquick/pokie/PokieApplicationTests.java
@@ -2,7 +2,6 @@ package com.tlquick.pokie;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import javax.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
